99799bd74a37ac6e8ec1612b27ba1d155b3b8a32
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-26 16:03:40 -0400

    set up structure of class App

---
+++ src/components/TodoForm.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            input: ""
+        }
+    }
+
+    render() {
+        return (
+            <form>
+                
+            </form>
+        )
+    }
+}
+
+export default TodoForm
